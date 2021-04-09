00d53c8bdf88f3e45e057fc48dbedbfff59b2a85
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-04-09 20:11:03 -0300

    Criar repositório da categoria

---
+++ src/main/java/com/br/zup/projetocontrolefinanceiro/repositories/CategoriaRepository.java
@@ -0,0 +1,6 @@
+package com.br.zup.projetocontrolefinanceiro.repositories;
+
+import com.br.zup.projetocontrolefinanceiro.models.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaRepository extends CrudRepository<Categoria, Integer> { }
