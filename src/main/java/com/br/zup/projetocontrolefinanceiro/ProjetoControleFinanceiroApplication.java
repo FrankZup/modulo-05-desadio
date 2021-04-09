466527ff7b2dd21addc2a100e9b7fa9ade3da59a
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-04-09 19:46:11 -0300

    Criar configuração com o banco de dados

---
+++ src/main/java/com/br/zup/projetocontrolefinanceiro/ProjetoControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.projetocontrolefinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoControleFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoControleFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/zup/projetocontrolefinanceiro/ProjetoControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zup.projetocontrolefinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
