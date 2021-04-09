8884a6000af918855b1402c7226a3ddeae12876b
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-04-09 20:16:05 -0300

    Criar repositório do saldo

---
+++ src/main/java/com/br/zup/projetocontrolefinanceiro/repositories/SaldoRepository.java
@@ -0,0 +1,6 @@
+package com.br.zup.projetocontrolefinanceiro.repositories;
+
+import com.br.zup.projetocontrolefinanceiro.models.Saldo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SaldoRepository extends CrudRepository<Saldo, String> { }
